4c2ae3cf354ad5e4a41df0c7810241ce590bbf9c
Admin <lol_et.ai@bk.ru>
2024-07-01 18:03:27 +0300

    Make 2 lab

--- 2_lab_main.py
+++ 2_lab_main.py
@@ -1,2 +1,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
+y1 = [1, 2]
+y2 = [2, 3]
+y3 = [3, 4]
+
+# y(x) = sin(x) * (x ** 2), x  = [0; 20; 0.01]
+
+X  = np.arange(0, 20, 0.01)
+Y = np.sin(X) * (X ** 2)
+
+plt.plot(Y)
+plt.grid(True)
+plt.xlabel('x')
+plt.ylabel('y')
+plt.show()
